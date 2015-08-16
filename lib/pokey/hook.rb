a8a0e26b0b17502bb68acbf13507f4d7c303a45a
Chuck Callebs <chuck@callebs.io>
2015-08-16 18:38:31 -0400

    Add/fix specs for Pokey::Hook.

--- lib/pokey/hook.rb
+++ lib/pokey/hook.rb
@@ -1,6 +1,11 @@
 class Pokey::Hook
   attr_accessor :destination, :http_method, :data, :interval
 
+  def initialize
+    @http_method = :post
+    @data = {}
+  end
+
   def make_http_request!
     Pokey::Request.make!(destination, http_method, data)
   end
--- spec/pokey/hook_spec.rb
+++ spec/pokey/hook_spec.rb
@@ -3,6 +3,16 @@ require './lib/pokey'
 RSpec.describe Pokey::Hook do
   subject { Pokey::Hook.new }
 
+  describe "#initialize" do
+    it "defaults #http_method to :post" do
+      expect(subject.http_method).to eq :post
+    end
+
+    it "defaults #data to empty hash" do
+      expect(subject.data).to eq({})
+    end
+  end
+
   context "when subclassed" do
     subject do
       class TestHook < Pokey::Hook
@@ -11,7 +21,7 @@ RSpec.describe Pokey::Hook do
         end
 
         def http_method
-          :post
+          :get
         end
 
         def data
@@ -25,7 +35,12 @@ RSpec.describe Pokey::Hook do
       TestHook.new
     end
 
-    context '#make_http_request!' do
+    it "uses overridden parameters instead of default values" do
+      expect(subject.http_method).to eq(:get)
+      expect(subject.data).to eq({ name: "Test Hook", id: 1 })
+    end
+
+    describe '#make_http_request!' do
       it 'attempts to make an http request' do
         expect(Pokey::Request).to receive(:make!)
         subject.make_http_request!
